c8a8107dbaa88ebb4299874aaafe36edb093a2ca
Merlin Nuñez <merlinsn@gmail.com>
2021-12-14 21:01:03 -0300

    CM-224 Implementar auditoria en users-be

--- src/main/java/com/cema/users/services/validation/administration/AdministrationClientService.java
+++ src/main/java/com/cema/users/services/validation/administration/AdministrationClientService.java
@@ -1,6 +1,10 @@
 package com.cema.users.services.validation.administration;
 
+import com.cema.users.domain.audit.Audit;
+
 public interface AdministrationClientService {
 
     void validateEstablishment(String cuig);
+
+    void sendAuditRequest(Audit audit);
 }
